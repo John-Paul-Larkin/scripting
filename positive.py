3d0ba7931683bd40e462ef2a130078f7e41969f6
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2024-11-20 19:48:56 +0000

    py class

---
+++ positive.py
@@ -0,0 +1,15 @@
+
+
+
+def most_frequent_element_in_list():
+   my_list = [1, 2, 2, 3, 4, 4, 5,19, 100,200,199999]
+
+   unique_list = list(set(my_list))
+
+   print(unique_list)  
+
+
+
+if __name__ == "__main__":  
+   most_frequent_element_in_list()
+   
